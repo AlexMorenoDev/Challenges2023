c0b4ad2917bb6f903a0a4a727935c077f1d07b7a
AlexMorenoDev <alx.moreno.lobato@gmail.com>
2023-06-12 18:33:11 +0200

    New challenge 'TheDatabase'

---
+++ TheDatabase/main.py
@@ -0,0 +1,27 @@
+"""
+ * Realiza una conexión desde el lenguaje que hayas seleccionado a la siguiente base de datos MySQL:
+ * - Host: mysql-5707.dinaserver.com
+ * - Port: 3306
+ * - User: mouredev_read
+ * - Password: mouredev_pass
+ * - Database: moure_test
+ * 
+ * Una vez realices la conexión, lanza la siguiente consulta e imprime el resultado:
+ * - SELECT * FROM `challenges`
+ *
+ * Se pueden usar librerías para realizar la lógica de conexión a la base de datos.
+"""
+
+import mysql.connector
+
+mysql_db = mysql.connector.connect(
+    host="mysql-5707.dinaserver.com",
+    port="3306",
+    user="mouredev_read",
+    password="mouredev_pass",
+    database="moure_test"
+)
+
+db_cursor = mysql_db.cursor()
+db_cursor.execute("SELECT * FROM challenges")
+print(db_cursor.fetchall())
