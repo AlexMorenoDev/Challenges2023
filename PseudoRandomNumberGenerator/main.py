6b08c8e2fe78f5b3a4f60d170f7e60c37527f722
AlexMorenoDev <alx.moreno.lobato@gmail.com>
2023-06-12 19:38:16 +0200

    Changing seed in 'PseudoRandomNumberGenerator' so that it is smaller than 'm'

--- PseudoRandomNumberGenerator/main.py
+++ PseudoRandomNumberGenerator/main.py
@@ -2,12 +2,12 @@
  * Crea un generador de números pseudoaleatorios entre 0 y 100.
  * - No puedes usar ninguna función "random" (o semejante) del lenguaje de programación seleccionado.
 """
-import time
+from datetime import datetime
 
 # Linear congruential generator
 def custom_random():
     # (a * X + c) mod m --> We add '% 101' because we are searching for a number between 0 and 100
-    return ((1103515245 * time.time_ns() + 12345) % (2**31)) % 101
+    return ((1103515245 * datetime.now().microsecond + 12345) % (2**31)) % 101
 
 
 def main():
